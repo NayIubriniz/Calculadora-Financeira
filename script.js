36cf5c65f070c0fdc0c4469415c1b1f41b343d36
Nayra Rodrigues <nayra_iubriniz@outlook.com>
2024-06-12 19:29:07 -0300

    calculadora de juros compostos atualizada

--- script.js
+++ script.js
@@ -1,36 +1,10 @@
-function calcularValorFuturo(){
-    const quantiaInicial = parseFloat(document.getElementById('quantiaInicial').value);
+document.getElementById('calculate').addEventListener('click', function(){
+    const quantiaInicial = document.getElementById('quantiaInicial').value
+    const taxaJuros = (document.getElementById('taxaJuros').value / 100)
+    const time = document.getElementById('time').value
+   
 
-    const taxaJuros = parseFloat(document.getElementById('taxaJuros').value / 100);
-
-    const anos = parseFloat(document.getElementById('anos').value);
-
-    const periodo = document.getElementById('periodo').value;
-
-    //---------------------------------------
-
-    if(isNaN (quantiaInicial) || isNaN(taxaJuros) || isNaN(anos)){
-        document.getElementById('resultado').innerText = `Por favor, insira valores válidos`;
-        return;
-    }
-
-    const totalMeses = periodo ===  'anual' ? anos * 12 : anos;
-    const taxaMensal = taxaJuros / 12;
-    const valorFuturo = quantiaInicial * Math.pow((1 + taxaMensal), totalMeses);
-    document.getElementById('resultado').innerText = `Valor Futuro: R$${valorFuturo.toFixed(2)}`;
-
-}
-
-document.getElementById('periodo').addEventListener('change', function(){
-    const periodo = document.getElementById('periodo').value;
-    const anosInput =document.getElementById('anos');
-    const mesesInput = document.getElementById('meses');
-
-    if(periodo === 'anual'){
-        anosInput.required = true;
-        mesesInput.required = false;
-    }else if (periodo === 'mensal'){
-        anosInput.required = false;
-        mesesInput.required = true;
-    }
+    const total = quantiaInicial * (1 + taxaJuros)**time;
+    
+    document.getElementById('total').innerHTML = ('R$ ' + total.toFixed(2).replace('.',','));
 });
